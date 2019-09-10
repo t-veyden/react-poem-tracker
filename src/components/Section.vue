71f179204f8db72ee8e9ae694afdc5557a1fe418
tdyachenko <luftaverse@gmail.com>
2019-09-10 17:51:02 +0300

    Adds Section component

---
+++ src/components/Section.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{sectionTitle}}</h2>
+    <slot></slot>
+    <router-link to="/" v-if="typeOfBar">see more</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Section",
+
+    props: {
+      sectionTitle: String,
+      typeOfBar: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
