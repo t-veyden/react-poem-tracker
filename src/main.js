b2a1f1aa580cc7f57ad204c01010918fcd314972
tdyachenko <luftaverse@gmail.com>
2019-09-10 17:46:37 +0300

    Imports VueResource to main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource);
 
 new Vue({
   router,
