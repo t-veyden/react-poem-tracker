6584c27f03f8f93d8ef9b04d91b477f0c564d6bc
tdyachenko <luftaverse@gmail.com>
2019-09-10 17:49:49 +0300

    Adds mock Bars component

---
+++ src/components/Bars.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="t-bar">
+      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
+    </div>
+    <div class="t-bar">
+      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
+    </div>
+    <div class="t-bar">
+      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Bars",
+    data() {
+      return {
+        bars: []
+      }
+    },
+
+    props: {
+      type: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .t-bar {
+    padding: 10px;
+
+    &:nth-child(odd) {
+      background-color: #ededed;
+    }
+  }
+</style>
