4d4499943c639503de44b1caae30dad170c59cb3
tdyachenko <luftaverse@gmail.com>
2019-09-10 17:51:52 +0300

    Adds crude version of Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+    <h1>Until changed</h1>
+    <t-section sectionTitle="In Progress">
+      <div style="width:400px">
+        <v-carousel hide-delimiters show-arrows-on-hover height="350">
+          <v-carousel-item v-for="(poem, idx) in poems" :key="idx">
+            <t-poem :poemData="poem"/>
+          </v-carousel-item>
+        </v-carousel>
+      </div>
+    </t-section>
+    <t-section sectionTitle="Purgatory" typeOfBar>
+      <t-bars type="purgatory"/>
+    </t-section>
+    <t-section sectionTitle="Completed" typeOfBar>
+      <t-bars type="completed"/>
+    </t-section>
   </div>
 </template>
+
+<script>
+  import TSection from '../components/Section'
+  import TBars from '../components/Bars'
+  import TPoem from '../components/Poem'
+
+  export default {
+    components: {
+      TSection,
+      TPoem,
+      TBars
+    },
+
+    data() {
+      return {
+        poems: []
+      }
+    },
+
+    created() {
+      this.getData()
+    },
+
+    methods: {
+      getData() {
+        this.$http.get('http://localhost:3000/purgatory')
+          .then(response => {
+            this.poems = response.body
+            // console.log(response.body);
+          })
+      }
+    }
+  }
+</script>
 
