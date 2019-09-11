3ecec9061599ea62424abb007bc8a900ce50fc44
tdyachenko <luftaverse@gmail.com>
2019-09-12 00:06:25 +0300

    Adds links to poems in Bars

--- src/components/Bars.vue
+++ src/components/Bars.vue
@@ -1,56 +1,60 @@
 <template>
   <div>
     <div class="t-bar" v-for="poem in poemBars" :key="poem.id">
-      <div><router-link to="/">{{poem.title}}</router-link><span> || </span><small>{{poem.author}}</small></div>
+      <div>
+        <router-link :to="`/poem/${poem.id}`">{{poem.title}}</router-link>
+        <span>||</span>
+        <small>{{poem.author}}</small>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Bars",
-    data() {
-      return {
-        bars: [],
-        barsLength: 3
-      }
-    },
+export default {
+  name: "Bars",
+  data() {
+    return {
+      bars: [],
+      barsLength: 3
+    };
+  },
 
-    props: {
-      type: String,
-      poems: Array
-    },
+  props: {
+    type: String,
+    poems: Array
+  },
 
-    computed: {
-      poemBars() {
-        if(this.poems && this.poems.length <= this.barsLength) {
-          return this.poems;
-        } else {
-          return this.getRandomPoems(this.poems, this.barsLength);
-        }
-      }
-    },
-
-    methods: {
-      getRandomPoems(arr, num) {
-        const bars = [];
-
-        while (bars.length < num) {
-          const el = arr[Math.floor(Math.random() * arr.length)];
-          bars.indexOf(el) === -1 ? bars.push(el) : null;
-        }
-        return bars;
+  computed: {
+    poemBars() {
+      if (this.poems && this.poems.length <= this.barsLength) {
+        return this.poems;
+      } else {
+        return this.getRandomPoems(this.poems, this.barsLength);
       }
     }
+  },
+
+  methods: {
+    getRandomPoems(arr, num) {
+      const bars = [];
+
+      while (bars.length < num) {
+        const el = arr[Math.floor(Math.random() * arr.length)];
+        bars.indexOf(el) === -1 ? bars.push(el) : null;
+      }
+      return bars;
+    }
   }
+};
 </script>
 
 <style lang="scss" scoped>
-  .t-bar {
-    padding: 10px;
+.t-bar {
+  padding: 10px;
 
-    &:nth-child(odd) {
-      background-color: #ededed;
-    }
+  &:nth-child(odd) {
+    background-color: #ededed;
   }
+}
 </style>
