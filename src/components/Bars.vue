049c27599c61c662b02837d6277706ed7bb77898
tdyachenko <luftaverse@gmail.com>
2019-09-11 18:29:23 +0300

    Restricts Bars length

--- src/components/Bars.vue
+++ src/components/Bars.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
-    <div class="t-bar">
-      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
-    </div>
-    <div class="t-bar">
-      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
-    </div>
-    <div class="t-bar">
-      <div><router-link to="/">Something something llama</router-link><span> || </span><small>someone</small></div>
+    <div class="t-bar" v-for="poem in poemBars" :key="poem.id">
+      <div><router-link to="/">{{poem.title}}</router-link><span> || </span><small>{{poem.author}}</small></div>
     </div>
   </div>
 </template>
@@ -17,12 +11,36 @@
     name: "Bars",
     data() {
       return {
-        bars: []
+        bars: [],
+        barsLength: 3
       }
     },
 
     props: {
-      type: String
+      type: String,
+      poems: Array
+    },
+
+    computed: {
+      poemBars() {
+        if(this.poems && this.poems.length <= this.barsLength) {
+          return this.poems;
+        } else {
+          return this.getRandomPoems(this.poems, this.barsLength);
+        }
+      }
+    },
+
+    methods: {
+      getRandomPoems(arr, num) {
+        const bars = [];
+
+        while (bars.length < num) {
+          const el = arr[Math.floor(Math.random() * arr.length)];
+          bars.indexOf(el) === -1 ? bars.push(el) : null;
+        }
+        return bars;
+      }
     }
   }
 </script>
