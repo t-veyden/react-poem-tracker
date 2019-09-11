53abb801e06ff34e4bb57287e993aed523e3c2df
tdyachenko <luftaverse@gmail.com>
2019-09-12 00:09:41 +0300

    Adds Poem view

--- src/views/Poem.vue
+++ src/views/Poem.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="about">
+  <div class="poem-view">
     <h1>This is a single poem page</h1>
+    <t-poem :poemData="poem" />
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Poem"
+import TPoem from "../components/Poem";
+
+export default {
+  name: "Poem",
+  data() {
+    return {
+      poem: {},
+      id: this.$route.params.id
+    };
+  },
+
+  components: {
+    TPoem
+  },
+
+  created() {
+    this.getPoem(this.id);
+  },
+
+  methods: {
+    getPoem(id) {
+      this.$http.get(`http://localhost:3000/poems/${id}`).then(response => {
+        this.poem = response.body;
+      });
+    }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
