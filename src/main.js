43c1cd57f0238ba2ae9aabb46a6afd23c5bfdad1
tdyachenko <luftaverse@gmail.com>
2019-09-30 00:18:38 +0300

    Adds wysiwyg plugin

--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import router from './router';
 import store from './store/store';
 import vuetify from './plugins/vuetify';
 import VueResource from 'vue-resource';
+import wysiwyg from "vue-wysiwyg";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
+
+Vue.use(wysiwyg, {
+  hideModules: {
+    code: true,
+    table: true,
+    headings: true,
+    link: true,
+    orderedList: true,
+    unorderedList: true,
+    image: true,
+    separator: true,
+    underline: true
+  },
+  forcePlainTextOnPaste: true
+});
 
 new Vue({
   router,
