1c12ccd6c9a243c8a6bd387f41c4dabcfbee77ae
tdyachenko <luftaverse@gmail.com>
2020-01-24 16:53:10 +0300

    Inits AuthorsList view

---
+++ src/views/AuthorsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(author, idx) in authorsList" :key="idx">
+      <!-- <router-link>{{ author }}</router-link> -->
+      {{ author.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  created() {
+    this.$store.dispatch('poems/getPoemsData');
+  },
+
+  computed: {
+    ...mapGetters('poems', ['authorsList'])
+  }
+};
+</script>
