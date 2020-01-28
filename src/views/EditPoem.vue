4bd2bcbc782a412855b66e1dbbc7b5b67c652270
tdyachenko <luftaverse@gmail.com>
2020-01-28 17:23:06 +0300

    Updates EditPoem to react(ish) style

--- src/views/EditPoem.vue
+++ src/views/EditPoem.vue
@@ -27,131 +27,134 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Watch } from 'vue-property-decorator';
+import { mixins } from 'vue-class-component';
 import { mapState } from 'vuex';
 import { uxMixin, textMixin } from '../utils';
 import { defineID } from '../utils/helpers';
 
-export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-      updPoem: {
-        title: '',
-        author: {
-          name: '',
-          id: ''
-        },
-        text: '',
-        lang: ''
-      },
-      snackbarMessage: 'Update was successful',
-      langs: ['English', 'French', 'Spanish'],
-      rules: [v => !!v || "This field shouldn't be empty"]
-    };
-  },
+interface SinglePoem {
+  text: string[];
+  author: object;
+}
 
-  mixins: [uxMixin, textMixin],
-
-  computed: {
-    ...mapState('poems', ['poem']),
-
-    isChanged() {
-      if (this.isLoaded) {
-        const keys = Object.keys(this.updPoem);
-        const nestedKeys = Object.keys(this.updPoem.author);
-        keys.splice(keys.indexOf('author'), 1);
-
-        const differ = this.checkDiff(['updPoem'], ['poem']);
-        const nestedDiffer = this.checkDiff(['updPoem', 'author'], ['poem', 'author']);
-
-        return keys.some(differ) || nestedKeys.some(nestedDiffer);
-      } else return false;
+@Component({
+  name: 'EditPoem',
+  computed: mapState('poems', ['poem'])
+})
+export default class EditPoem extends mixins(uxMixin, textMixin) {
+  public poem!: SinglePoem;
+  id: string = '';
+  updPoem = {
+    title: '',
+    author: {
+      name: '',
+      id: ''
     },
+    text: '',
+    lang: ''
+  };
+  snackbarMessage = 'Update was successful';
+  langs = ['English', 'French', 'Spanish'];
+  rules = [v => !!v || "This field shouldn't be empty"];
 
-    isLoaded() {
-      return this.poem.author !== undefined;
-    }
-  },
-
-  created() {
-    this.$store.dispatch('poems/getPoemsData');
-    this.$store.dispatch('poems/getSinglePoem', this.id);
-  },
-
-  watch: {
-    poem(val) {
-      const keys = Object.keys(this.updPoem);
-      if (val) {
-        keys.forEach(key => {
-          if (key !== 'text' && key !== 'author') {
-            this.updPoem[key] = this.poem[key];
-          }
-        });
-
-        this.updPoem.author = Object.assign(
-          this.updPoem.author,
-          this.poem.author
-        );
-
-        if (Array.isArray(this.poem.text)) {
-          const poemString = this.poem.text
-            .map(line => `<div>${line}</div>`)
-            .join('');
-          this.updPoem.text = poemString;
-        } else {
-          this.updPoem.text = this.poem.text;
+  @Watch('poem')
+  handler(val) {
+    const keys = Object.keys(this.updPoem);
+    if (val) {
+      keys.forEach(key => {
+        if (key !== 'text' && key !== 'author') {
+          this.updPoem[key] = this.poem[key];
         }
-      }
-    }
-  },
+      });
 
-  methods: {
-    updateID() {
-      this.updPoem.author.id = defineID(this.updPoem.author.name);
-    },
-
-    checkDiff(x, y) {
-      return (key) => {
-        return this.getPath(x)[key] !== this.getPath(y)[key];
-      }
-    },
-
-    getPath(keyArr) {
-      const path = keyArr.reduce((acc, key) => {
-        return acc[key];
-      }, this)
-      return path;
-    },
-
-    submitPoem() {
-      if (this.$refs.editForm.validate() && this.updPoem.text) {
-        this.$store.dispatch('poems/updatePoemInfo', {
-          id: this.id,
-          body: this.updPoem
-        });
-
-        this.showMessage(this.snackbarMessage);
-        setTimeout(() => {
-          this.$router.go(-1);
-        }, 2000);
-      }
-    },
-
-    cancelEdit() {
-      this.isChanged ? this.confirmCancel() : this.$router.go(-1);
-    },
-
-    confirmCancel() {
-      const confirmCancel = confirm(
-        'You have unsaved changes. Are you sure you wanna cancel?'
+      this.updPoem.author = Object.assign(
+        this.updPoem.author,
+        this.poem.author
       );
-      if (confirmCancel) {
-        this.$router.go(-1);
-      } else return;
+
+      if (Array.isArray(this.poem.text)) {
+        const poemString = this.poem.text
+          .map(line => `<div>${line}</div>`)
+          .join('');
+        this.updPoem.text = poemString;
+      } else {
+        this.updPoem.text = this.poem.text;
+      }
     }
   }
-};
+
+  get isChanged() {
+    if (this.isLoaded) {
+      const keys = Object.keys(this.updPoem);
+      const nestedKeys = Object.keys(this.updPoem.author);
+      keys.splice(keys.indexOf('author'), 1);
+
+      const differ = this.checkDiff(['updPoem'], ['poem']);
+      const nestedDiffer = this.checkDiff(
+        ['updPoem', 'author'],
+        ['poem', 'author']
+      );
+
+      return keys.some(differ) || nestedKeys.some(nestedDiffer);
+    } else return false;
+  }
+
+  get isLoaded() {
+    return this.poem.author !== undefined;
+  }
+
+  created() {
+    this.id = this.$route.params.id;
+    this.$store.dispatch('poems/getPoemsData');
+    this.$store.dispatch('poems/getSinglePoem', this.id);
+  }
+
+  updateID() {
+    this.updPoem.author.id = defineID(this.updPoem.author.name);
+  }
+
+  checkDiff(x: string[], y: string[]) {
+    return key => {
+      return this.getPath(x)[key] !== this.getPath(y)[key];
+    };
+  }
+
+  getPath(keyArr: string[]) {
+    const path = keyArr.reduce((acc, key) => {
+      return acc[key];
+    }, this);
+    return path;
+  }
+
+  submitPoem() {
+    if (this.$refs.editForm.validate() && this.updPoem.text) {
+      this.$store.dispatch('poems/updatePoemInfo', {
+        id: this.id,
+        body: this.updPoem
+      });
+
+      this.showMessage(this.snackbarMessage);
+      setTimeout(() => {
+        this.$router.go(-1);
+      }, 2000);
+    }
+  }
+
+  cancelEdit() {
+    this.isChanged ? this.confirmCancel() : this.$router.go(-1);
+  }
+
+  confirmCancel() {
+    const confirmCancel = confirm(
+      'You have unsaved changes. Are you sure you wanna cancel?'
+    );
+    if (confirmCancel) {
+      this.$router.go(-1);
+    } else return;
+  }
+}
 </script>
 
 <style scoped>
