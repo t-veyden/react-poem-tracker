07db68c688bdda8cb3d45487a57230182b851d65
tdyachenko <luftaverse@gmail.com>
2020-01-30 17:24:08 +0300

    Cleans up imports in Author view

--- src/views/Author.vue
+++ src/views/Author.vue
@@ -11,8 +11,7 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import MapVuex from '../customDecorators';
 import { mapState, mapGetters } from 'vuex';
 
