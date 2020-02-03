0ecff545f6f373277e08bd508b4c8a7574484e90
tdyachenko <luftaverse@gmail.com>
2020-02-03 13:45:52 +0300

    Fixes searchMixin to accept either string or object as target

--- src/utils/searchMixin.ts
+++ src/utils/searchMixin.ts
@@ -1,24 +1,32 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+interface SearchOptions {
+  query: string;
+  searchBy: string;
+}
+
 @Component({
   name: 'searchMixin'
 })
 export default class searchMixin extends Vue {
-  search: string = "";
-  searchBy: string = "title";
-  dataStore: string = "";
+  search: string = '';
+  searchBy: string = 'title';
+  dataStore: string = '';
 
-  get filteredPoems() {
-    return this[this.dataStore].filter((poem: object) => {
-      return poem[this.searchBy]
-        .toLowerCase()
-        .includes(this.search.toLowerCase());
-    });
+  get filteredPoems(): object {
+    return this[this.dataStore].filter(
+      <T extends object, U extends keyof T>(poem: T) => {
+        const key: U = this.searchBy;
+        const target: string =
+          poem[key] instanceof Object ? poem[key].name : poem[key];
+        return target.toLowerCase().includes(this.search.toLowerCase());
+      }
+    );
   }
 
-  findPoem({ query, searchBy }: {query: string, searchBy: string}) {
+  findPoem({ query, searchBy }: SearchOptions) {
     this.search = query;
     this.searchBy = searchBy;
   }
-};
+}
