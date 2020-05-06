d7d35b36d16e92a56a9d560f62bd7f5b3b807512
tanya veyden <luftaverse@gmail.com>
2020-05-06 18:35:41 +0300

    Adds page header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './header.scss';
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header = ({title}: HeaderProps) => (
+  <h1 className="header">
+    {title}
+  </h1>
+);
+
+export default Header;
