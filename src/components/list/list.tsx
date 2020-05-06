e195c180a88e528b815d5c2d06c3e8893155e1c4
tanya veyden <luftaverse@gmail.com>
2020-05-06 19:08:27 +0300

    Adds list component

---
+++ src/components/list/list.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {singlePoem} from '../../utils/common-types';
+
+import './list.scss';
+
+interface ListProps {
+  poems: singlePoem[];
+}
+
+function List({poems}: ListProps) {
+  return (
+    <ul className="list">
+      {
+        poems.map(poem => (
+          <li className="list__item" key={poem.id}>
+            <span>link_here_{poem.title}</span>
+            <div className="list-item__author">
+              _by_<span> link_here_{poem.author.name}</span>
+            </div>
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+export default List;
