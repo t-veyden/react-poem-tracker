ede263bc1e2ac67d69c9e8bf4ff12e0ec386c6d3
tanya veyden <luftaverse@gmail.com>
2020-05-13 11:48:35 +0300

    Adds routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
-import HomeView from "./views/home";
+import HomeView from './views/home';
+import Purgatory from './views/purgatory';
+
+import {poems} from './temp-data';
 
 function App() {
-  return <div className="App">
-    <HomeView/>
-  </div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path={'/'} component={HomeView}/>
+        <Route
+          exact
+          path={'/purgatory'}
+          render={(props) => <Purgatory {...props} data={poems}/>}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
