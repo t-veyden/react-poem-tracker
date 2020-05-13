245813cd83aaf57e68279c18fafa004f8aca80a2
tanya veyden <luftaverse@gmail.com>
2020-05-13 11:47:47 +0300

    Adds mock data

---
+++ src/temp-data.js
@@ -0,0 +1,329 @@
+export const poems = [
+  {
+    "id": "asdasd",
+    "author": {
+      "name": "robert frost",
+      "id": "4534535"
+    },
+    "title": "fire and ice",
+    "text": [
+      "Some say the world will end in fire,",
+      "Some say in ice.",
+      "From what I've tasted of desire",
+      "I hold with those who favor fire.",
+      "But if it had to perish twice,",
+      "I think I know enough of hate",
+      "To say that for destruction ice",
+      "Is also great",
+      "And would suffice."
+    ],
+    "completed": false,
+    "in_progress": true,
+    "lang": "english"
+  },
+  {
+    "id": "skdksjd",
+    "author": {
+      "name": "shakespeare",
+      "id": "345346df"
+    },
+    "title": "sonnet 130",
+    "text": [
+      "My mistress' eyes are nothing like the sun;",
+      "Coral is far more red than her lips' red;",
+      "If snow be white, why then her breasts are dun;",
+      "If hairs be wires, black wires grow on her head.",
+      "I have seen roses damask'd, red and white,",
+      "But no such roses see I in her cheeks;",
+      "And in some perfumes is there more delight",
+      "Than in the breath that from my mistress reeks.",
+      "I love to hear her speak, yet well I know",
+      "That music hath a far more pleasing sound;",
+      "I grant I never saw a goddess go;",
+      "My mistress, when she walks, treads on the ground:",
+      "And yet, by heaven, I think my love as rare",
+      "As any she belied with false compare."
+    ],
+    "completed": false,
+    "in_progress": true,
+    "lang": "english"
+  },
+  {
+    "id": "skdksjdfsdf",
+    "author": {
+      "name": "pablo neruda",
+      "id": "4567567njh"
+    },
+    "title": "soneto xvii",
+    "text": [
+      "No te amo como si fueras rosa de sal, topacio",
+      "o flecha de claveles que propagan el fuego:",
+      "te amo como se aman ciertas cosas oscuras,",
+      "secretamente, entre la sombra y el alma.",
+      "",
+      "Te amo como la planta que no florece y lleva",
+      "dentro de sí, escondida, la luz de aquellas flores,",
+      "y gracias a tu amor vive oscuro en mi cuerpo",
+      "el apretado aroma que ascendió de la tierra.",
+      "",
+      "Te amo sin saber cómo, ni cuándo, ni de dónde,",
+      "te amo directamente sin problemas ni orgullo:",
+      "así te amo porque no sé amar de otra manera,",
+      "",
+      "sino así de este modo en que no soy ni eres,",
+      "tan cerca que tu mano sobre mi pecho es mía,",
+      "tan cerca que se cierran tus ojos con mi sueño."
+    ],
+    "completed": false,
+    "in_progress": true,
+    "lang": "spanish"
+  },
+  {
+    "id": "2sdfwe",
+    "author": {
+      "name": "someone",
+      "id": "retertert45"
+    },
+    "title": "smth smth smth...",
+    "text": "<div>smth smth smth</div><div>smth smth else</div>",
+    "completed": false,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "cvxcvxc",
+    "author": {
+      "name": "nobody",
+      "id": "45456gkhl"
+    },
+    "title": "cats",
+    "text": [
+      "cats are everywhere",
+      "descending"
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "sdfsdfs",
+    "author": {
+      "name": "nobody",
+      "id": "45456gkhl"
+    },
+    "title": "silence",
+    "text": [
+      "...",
+      ".."
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "german"
+  },
+  {
+    "id": "sdfsdfssddsd",
+    "author": {
+      "name": "nobody",
+      "id": "45456gkhl"
+    },
+    "title": "suddenly",
+    "text": [
+      "suddenly",
+      "there's another index"
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "sdfs4545ssddsd",
+    "author": {
+      "name": "nobody",
+      "id": "45456gkhl"
+    },
+    "title": "more suddenly",
+    "text": [
+      "even more suddenly",
+      "there is one index more"
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "2sdfwesdsd",
+    "author": {
+      "name": "someone2",
+      "id": "345hjgj"
+    },
+    "title": "***",
+    "text": [
+      "smth smth smth",
+      "smth smth else",
+      "and also something else"
+    ],
+    "completed": true,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "cvxcvxcasdad",
+    "author": {
+      "name": "nobody2",
+      "id": "dfgdfg45454"
+    },
+    "title": "more cats",
+    "text": [
+      "cats are everywhere",
+      "everywhere",
+      "descending"
+    ],
+    "completed": true,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "sdfsdfswewee",
+    "author": {
+      "name": "nobody2",
+      "id": "dfgdfg45454"
+    },
+    "title": "more silence",
+    "text": [
+      "...",
+      "...",
+      ".."
+    ],
+    "completed": true,
+    "in_progress": false,
+    "lang": "french"
+  },
+  {
+    "id": "sdsd';lpo",
+    "author": {
+      "name": "e.e.cummings",
+      "id": "678sdfsdf"
+    },
+    "title": "i carry your heart",
+    "text": [
+      "i carry your heart with me(i carry it in",
+      "my heart)i am never without it(anywhere",
+      "i go you go,my dear;and whatever is done",
+      "by only me is your doing,my darling)",
+      "i fear",
+      "no fate(for you are my fate,my sweet)i want",
+      "no world(for beautiful you are my world,my true)",
+      "and it’s you are whatever a moon has always meant",
+      "and whatever a sun will always sing is you",
+      "",
+      "here is the deepest secret nobody knows",
+      "(here is the root of the root and the bud of the bud",
+      "and the sky of the sky of a tree called life;which grows",
+      "higher than soul can hope or mind can hide)",
+      "and this is the wonder that's keeping the stars apart",
+      "",
+      "i carry your heart(i carry it in my heart)"
+    ],
+    "completed": true,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "id": "kkhkjhkj",
+    "author": {
+      "name": "someone else",
+      "id": "678345345gh"
+    },
+    "title": "line",
+    "text": [
+      "line"
+    ],
+    "completed": true,
+    "in_progress": false,
+    "lang": "english"
+  },
+  {
+    "author": {
+      "name": "23423423",
+      "id": "sdf4354545kj"
+    },
+    "title": "234",
+    "text": "<div>dfgjlkgfj</div><div>123</div><div>1233455546</div>",
+    "completed": false,
+    "in_progress": false,
+    "lang": "English",
+    "id": "HAboRWt"
+  },
+  {
+    "author": {
+      "name": "8",
+      "id": "sdf435kj"
+    },
+    "title": "dsfsdfsdf sdfsdf sdf...",
+    "text": "<div>dsfsdfsdf sdfsdf sdfsdfsdf sdfsdfsdfdfsdf sdfsdfsdf sdfsdfsdf</div>6768678",
+    "completed": false,
+    "in_progress": false,
+    "lang": "English",
+    "id": "TIGn0nC"
+  },
+  {
+    "author": {
+      "name": "fgdfg",
+      "id": "dvsj6huu0rl"
+    },
+    "title": "title title",
+    "text": "dgdg<div>eljflf cat cat cat</div>",
+    "completed": false,
+    "in_progress": false,
+    "lang": "English",
+    "id": "s62dFsS"
+  },
+  {
+    "author": {
+      "name": "Михаил Гронас",
+      "id": "gdfjfghs56"
+    },
+    "title": "Когда мы придем к власти",
+    "text": [
+      "Когда мы придем к власти мы отменим",
+      "Все цвета кроме белого",
+      "Все события кроме встречи и смерти",
+      "Все слова и дела кроме прикосновений и междометий.",
+      "-",
+      "Вся власть снегу! Хижины и дворцы снегу!",
+      "Снег превыше всего! Да святится имя его!",
+      "Да приидет царствие снега",
+      "На ладонь на голову и на веки!"
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "Russian",
+    "id": "65756567"
+  }
+];
+
+export const own_poems = [
+  {
+    "author": {
+      "name": "me",
+      "id": "tv23424h"
+    },
+    "title": "some title",
+    "text": [
+      "some text",
+      "text"
+    ],
+    "completed": false,
+    "in_progress": false,
+    "lang": "english",
+    "id": "6575dfgd6567"
+  }
+];
+
+export const users = [
+  {
+    "id": "sdskllj",
+    "email": "sam@gmail.com",
+    "password": "skyshipboot"
+  }
+];
