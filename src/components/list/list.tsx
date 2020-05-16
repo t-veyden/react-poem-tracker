e65a3c9f554d23ddd09d3f9a994edd4e88a74985
tanya veyden <luftaverse@gmail.com>
2020-05-16 20:32:59 +0300

    Switches off TS (temporarily)

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 import {singlePoem} from '../../utils/common-types';
 
--- src/components/section-wrapper/section-wrapper.tsx
+++ src/components/section-wrapper/section-wrapper.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, {FunctionComponent} from 'react';
 
 import './section-wrapper.scss';
